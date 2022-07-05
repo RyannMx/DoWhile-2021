c4a633e55e1fe0f2eeaad2325ccfc03cddecdc06
Ryan Max <105246720+RyannMx@users.noreply.github.com>
2022-07-04 23:01:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+const LinksSocialMedia = {
+  github: 'RyannMx',
+  youtube: 'Null',
+  twitter: 'ryaanmx',
+  instagram: 'ryaanmx',
+  facebooke: 'Null'
+}
+
+function changeSocialMediaLinks(){
+  for (let li of socialLinks.children) {
+  const social = li.getAttribute
+  ('class')
+
+  li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+}
+  }
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  // alert(url)
+   
+  fetch(url)
+  .then(reponse =>  reponse.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userImage.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+}
+
+getGitHubProfileInfos()
